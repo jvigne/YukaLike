e0e06751d9dea3c688228589f365f199e4673cdd
Joanna Vigne <joanna.vigne@fabernovel.com>
2022-10-12 15:10:47 +0200

    openclassrooms version

--- YukaLike/ViewController.swift
+++ YukaLike/ViewController.swift
@@ -14,31 +14,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         getProduct("04963406")
     }
-
+    
     private func getProduct(_ code: String) {
-        Task {
-            do {
-                let product = try await HttpClient.shared.getProduct(code)
-                print("Product : \(product.productName)")
-            } catch let error as HttpClient.APIError {
-                handleAPIError(error: error)
-            } catch {
-                print("the server did not send back any response")
+        HttpClient.shared.getProduct(code) { product, apiError in
+            guard let product = product, apiError == nil else {
+                self.handleAPIError(error: apiError!)
+                return
             }
+            
+            print("Product : \(product.productName)")
         }
-        
     }
-//    v1
-//    private func getProduct(_ code: String) {
-//        HttpClient.shared.getProduct(code) { product, apiError in
-//            guard let product = product, apiError == nil else {
-//                self.handleAPIError(error: apiError!)
-//                return
-//            }
-//
-//            print("Product : \(product.productName)")
-//        }
-//    }
+
     
     private func handleAPIError(error: HttpClient.APIError) {
         switch error {
